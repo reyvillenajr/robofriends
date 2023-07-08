68d1553138b4aa93459970d75b8bb73ce332c2aa
Reynaldo Jr. Villena <support@raycomx.ca>
2023-07-08 19:56:19 -0400

    Changed website title

---
+++ src/robots.js
@@ -0,0 +1,63 @@
+export const robots = [
+    {
+      id: 1,
+      name: 'Rey Villena Jr.',
+      username: 'reyvillenajr',
+      email: 'reynaldovillenajr@gmail.com'
+    },
+    {
+      id: 2,
+      name: 'Ray Charles Villena',
+      username: 'Arcenio',
+      email: 'raycharlesvillena@gmail.com'
+    },
+    {
+      id: 3,
+      name: 'Maria Agnes Villena',
+      username: 'nesbil_1110',
+      email: 'nesbil_1110@yahoo.ca'
+    },
+    {
+      id: 4,
+      name: 'John Reynes Villena',
+      username: 'darkside',
+      email: 'johnreynesvillena1224@ymail.com'
+    },
+    {
+      id: 5,
+      name: 'April Jacob Rodriguez',
+      username: 'apeng',
+      email: 'apriljacob@gmail.com'
+    },
+    {
+      id: 6,
+      name: 'Mr. Reynaldo Villena Sr.',
+      username: 'reynaldovlln',
+      email: 'reynaldovlln@gmail.com'
+    },
+    {
+      id: 7,
+      name: 'Rutchel Villena',
+      username: 'ruthchel.villena',
+      email: 'rutchelvillena@gmail.com'
+    },
+    {
+      id: 8,
+      name: 'Caroline Pega',
+      username: 'Carol_Pega',
+      email: 'carolinepega.me'
+    },
+    {
+      id: 9,
+      name: 'Izzie Rae Villena',
+      username: 'izzie_rae',
+      email: 'izzieraevillena@gmail.com'
+    },
+    {
+      id: 10,
+      name: 'Ariana Grande',
+      username: 'Ari_G',
+      email: 'arianagrande@gmail.com'
+    }
+  ];
+  
